8c166df78bd88f9dcae801cca008a13128b60f68
Gordon Gauss <gordongauss@MacBook-Pro.local>
2015-05-22 16:41:05 -0700

    Men's Shoe Size Built

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    @IBOutlet weak var menShoeSizeTextField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        
+        var sizeFromTextField = menShoeSizeTextField.text.toInt()!
+        let conversionConstant = 30
+        mensConvertedShoeSizeLabel.hidden = false
+        mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+    
+    }
 
 }
 
